f3bbd1d3df2a9ff133ae690471d0352bc85cf232
Artur <danch718@gmail.com>
2018-07-31 20:34:57 +0200

    1

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ let _ = require('lodash');
 
 let array1 = [2, 5, 5, 1, 3, 1, 4, 2, 2, 4, 6];
 
-console.log(_.sortBy(_.uniq(array1)));
+let newArray1 = _.sortBy(_.uniq(array1));
+
+console.log(newArray1);
